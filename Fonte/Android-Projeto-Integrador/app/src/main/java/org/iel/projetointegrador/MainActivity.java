20dfb321d4d0862ff1c3b72af957afde2b710b0f
nosrednawall <nosrednawall@gmail.com>
2019-03-10 19:53:21 -0300

    android 100%

--- Fonte/Android-Projeto-Integrador/app/src/main/java/org/iel/projetointegrador/MainActivity.java
+++ Fonte/Android-Projeto-Integrador/app/src/main/java/org/iel/projetointegrador/MainActivity.java
@@ -2,6 +2,8 @@ package org.iel.projetointegrador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] alunos = {"Daniel","Ronaldo","Jeferson","Felipe" };
+
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_aluno);
+
+        //responsável por colocar as strings dentro das views
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,alunos);
+
+        //lista usa esse adapter
+        listaAlunos.setAdapter(adapter);
+
     }
 }
