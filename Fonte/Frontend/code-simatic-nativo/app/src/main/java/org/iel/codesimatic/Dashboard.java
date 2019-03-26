9f5948d975b5c992113301ff1289108a3f33db94
nosrednawall <nosrednawall@gmail.com>
2019-03-26 17:32:57 -0300

    adicionei as dependencias agora

--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/Dashboard.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/Dashboard.java
@@ -3,7 +3,14 @@ package org.iel.codesimatic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.*;
+
 public class Dashboard extends AppCompatActivity {
+
+    private XYPlot plot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
