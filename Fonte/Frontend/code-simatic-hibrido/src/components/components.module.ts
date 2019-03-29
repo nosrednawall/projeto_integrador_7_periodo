a2a8f7a9ef98cb1da0a54aa178d417201307d786
nosrednawall <nosrednawall@gmail.com>
2019-03-29 17:28:21 -0300

    removido todo o frontend para começar denovo

--- Fonte/Frontend/code-simatic-hibrido/src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { PlotComponent } from './plot/plot';
-@NgModule({
-	declarations: [PlotComponent],
-	imports: [],
-	exports: [PlotComponent]
-})
-export class ComponentsModule {}
