90e61e34f565a5563ad7bd7c112de4f0dbd54ad9
nosrednawall <nosrednawall@gmail.com>
2019-04-02 10:00:18 -0300

    ok

--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/LoginActivity.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/LoginActivity.java
@@ -1,9 +1,59 @@
 package org.iel.codesimatic.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.AutoCompleteTextView;
 
+import org.iel.codesimatic.R;
+
+import static org.iel.codesimatic.util.ValidacoesUtil.validaEmail;
+import static org.iel.codesimatic.util.ValidacoesUtil.validaSenha;
+
+/**
+ * Classe que controla a tela de login
+ */
 public class LoginActivity extends AppCompatActivity {
 
+    //variáveis que serão utilizadas
+    private String email;
+    private String senha;
 
+    /**
+     * Executado ao criar uma tela
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView)findViewById(R.id.email);
+    }
+
+    /**
+     * executado ao destruir uma tela
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+
+
+    /**
+     * valida os inputs
+     * @param email
+     * @param senha
+     * @return boolean
+     */
+    private boolean validaInputs(String email, String senha){
+        if(validaSenha(senha) && validaEmail(email)){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
---
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/util/ValidacoesUtil.java
@@ -0,0 +1,15 @@
+package org.iel.codesimatic.util;
+
+/**
+ * Classe que contém todas as validações
+ */
+public class ValidacoesUtil {
+
+    public static boolean validaSenha(String senha){
+        return true;
+    }
+
+    public static boolean validaEmail(String email){
+        return true;
+    }
+}
