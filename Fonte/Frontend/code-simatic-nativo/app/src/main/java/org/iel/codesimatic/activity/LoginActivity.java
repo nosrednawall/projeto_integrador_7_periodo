508d526f8348ba7b0ffbc548997c451f3f486ecb
nosrednawall <nosrednawall@gmail.com>
2019-04-02 09:28:11 -0300

    vamos fazer por partes esse login

---
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/LoginActivity.java
@@ -0,0 +1,9 @@
+package org.iel.codesimatic.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+}
+
--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/rest/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package org.iel.codesimatic.rest;
-
-
-/**
- * Classe responsável por setar o endereço do servidor rest e o tipo de conversor que será utilizado para deserializar os objetos enviados
- */
-public class RestApplication {
-
-}
