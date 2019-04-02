5b559a356b1fb9dd54bbad5809947793c3468587
nosrednawall <nosrednawall@gmail.com>
2019-04-02 15:49:19 -0300

    acabou a luz quase perdi tudo

--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/CadastroUsuarioActivity.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/CadastroUsuarioActivity.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import org.iel.codesimatic.R;
+import org.iel.codesimatic.model.Usuario;
 
 public class CadastroUsuarioActivity extends AppCompatActivity {
 
@@ -21,11 +24,25 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Toast.makeText(CadastroUsuarioActivity.this, "Usuário salvo com sucesso", Toast.LENGTH_SHORT).show();
                 finish();
-
             }
         });
 
+    }
+
+    private Usuario instanciaDadosTelaERetornaUsuario(){
+        EditText nomeCompletoEditText = (EditText) findViewById(R.id.cadastro_usuario_nome);
+        EditText cpfEditText = (EditText) findViewById(R.id.cadastro_usuario_cpf);
+        EditText setorEditText = (EditText) findViewById(R.id.cadastro_usuario_setor);
+        EditText ramalEditText = (EditText) findViewById(R.id.cadastro_usuario_ramal);
+        EditText emailEditText = (EditText) findViewById(R.id.cadastro_usuario_email);
+        EditText senhaEditText = (EditText) findViewById(R.id.cadastro_usuario_senha);
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.cadastro_usuario_radio_group);
+
+
+        return null;
     }
 }
