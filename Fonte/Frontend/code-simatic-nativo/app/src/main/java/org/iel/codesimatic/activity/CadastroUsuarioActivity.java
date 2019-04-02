974b2dbd29bbddc6ef55cf3828b980c4843a03da
nosrednawall <nosrednawall@gmail.com>
2019-04-02 11:38:50 -0300

    almoco

--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/CadastroUsuarioActivity.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/CadastroUsuarioActivity.java
@@ -2,6 +2,9 @@ package org.iel.codesimatic.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import org.iel.codesimatic.R;
 
@@ -12,5 +15,15 @@ public class CadastroUsuarioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //está criando uma instância para todos os componentes da tela
         setContentView(R.layout.activity_cadastro_usuario);
+
+        Button botaoSalvar = (Button) findViewById(R.id.cadastro_usuario_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CadastroUsuarioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
