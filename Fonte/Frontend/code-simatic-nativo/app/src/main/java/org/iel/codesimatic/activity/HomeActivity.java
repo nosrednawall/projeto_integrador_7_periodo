135b8c2e08a61b86663cedf9e9027e0c10d332b6
nosrednawall <nosrednawall@gmail.com>
2019-04-13 18:47:59 -0300

    ajustado a tela da home

--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/HomeActivity.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/HomeActivity.java
@@ -2,6 +2,9 @@ package org.iel.codesimatic.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Toast;
 
 import org.iel.codesimatic.R;
 
@@ -11,5 +14,19 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        CardView cardGraficos = (CardView) findViewById(R.id.home_card_graficos);
+        CardView cardConfiguracoes = (CardView) findViewById(R.id.home_card_configuracoes);
+        CardView cardUsuarios = (CardView) findViewById(R.id.home_card_usuarios);
+        CardView cardMaps = (CardView) findViewById(R.id.home_card_maps);
+        CardView cardLogoff = (CardView) findViewById(R.id.home_card_logoff);
+
+        cardLogoff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "Saindo...", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
     }
 }
