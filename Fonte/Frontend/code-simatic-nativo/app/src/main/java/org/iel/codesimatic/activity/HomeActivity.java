9c2b2ea64224a3bf3e7a3803cde547d3e623f113
nosrednawall <nosrednawall@gmail.com>
2019-04-14 11:30:41 -0300

    mostrando o maps

--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/HomeActivity.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/HomeActivity.java
@@ -1,13 +1,21 @@
 package org.iel.codesimatic.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 import org.iel.codesimatic.R;
 
+/**
+ * Autor:Anderson
+ */
 public class HomeActivity extends AppCompatActivity {
 
     @Override
@@ -15,12 +23,26 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //instancia os cards da tela
         CardView cardGraficos = (CardView) findViewById(R.id.home_card_graficos);
         CardView cardConfiguracoes = (CardView) findViewById(R.id.home_card_configuracoes);
         CardView cardUsuarios = (CardView) findViewById(R.id.home_card_usuarios);
         CardView cardMaps = (CardView) findViewById(R.id.home_card_maps);
         CardView cardLogoff = (CardView) findViewById(R.id.home_card_logoff);
 
+        //mostra o maps
+        cardMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri gmmIntentUri = Uri.parse("geo:-25.514541, -49.179868");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+            }
+        });
+
+        //efetua o logoff ao clicar no card
         cardLogoff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/MapsActivity.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/MapsActivity.java
@@ -1,14 +1,26 @@
 package org.iel.codesimatic.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.iel.codesimatic.R;
 
@@ -20,29 +32,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng iel = new LatLng(-25.514541, -49.179868);
+        mMap.addMarker(new MarkerOptions().position(iel).title("Marcado em Faculdades da indústria"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(iel));
     }
 }
