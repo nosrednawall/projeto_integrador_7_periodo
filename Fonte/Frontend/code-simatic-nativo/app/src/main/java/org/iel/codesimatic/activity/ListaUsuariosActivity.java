d6358aba6d478c7cab0760397d8fa0618453d0ae
nosrednawall <nosrednawall@gmail.com>
2019-04-14 19:37:52 -0300

    temos uma lista de fato

--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/ListaUsuariosActivity.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/ListaUsuariosActivity.java
@@ -2,9 +2,14 @@ package org.iel.codesimatic.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import org.iel.codesimatic.Adapter.LineAdapter;
 import org.iel.codesimatic.R;
+
+import java.util.ArrayList;
 
 public class ListaUsuariosActivity extends AppCompatActivity {
 
