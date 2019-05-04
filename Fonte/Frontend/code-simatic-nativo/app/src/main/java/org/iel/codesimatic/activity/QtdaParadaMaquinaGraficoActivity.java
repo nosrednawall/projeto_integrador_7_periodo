f24ef77a0a1b5215d166836d58e4023fb47ab1d2
nosrednawall <nosrednawall@gmail.com>
2019-05-04 20:18:28 -0300

    adicionado o locate do brasil

--- Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/QtdaParadaMaquinaGraficoActivity.java
+++ Fonte/Frontend/code-simatic-nativo/app/src/main/java/org/iel/codesimatic/activity/QtdaParadaMaquinaGraficoActivity.java
@@ -1,34 +1,38 @@
 package org.iel.codesimatic.activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import org.iel.codesimatic.R;
-import org.iel.codesimatic.model.QtdaVezesMaquinaParou;
+import org.iel.codesimatic.util.Util;
 
-import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
-public class QtdaParadaMaquinaGraficoActivity extends AppCompatActivity {
+public class QtdaParadaMaquinaGraficoActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     private LineChart chart;
     private SeekBar seekBarX;
     private TextView tvX;
+    private int count;
+    private float range;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +42,139 @@ public class QtdaParadaMaquinaGraficoActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_qtda_parada_maquina_grafico);
 
+        setTitle("Gráfico de Linhas");
 
+        tvX = findViewById(R.id.tvXMax);
+        seekBarX = findViewById(R.id.seekBar1);
+        seekBarX.setOnSeekBarChangeListener(this);
 
+        chart = findViewById(R.id.grafico_qtda_vezes_maquina_parou);
 
+        //sem texto de descrição
+        chart.getDescription().setEnabled(false);
 
+        //abilita o toque
+        chart.setTouchEnabled(true);
+
+        //boa pergunta
+        chart.setDragDecelerationFrictionCoef(0.9f);
+
+        //enable scaling and draggind or in other words abilita o scrol ????
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawGridBackground(false);
+        chart.setHighlightPerDragEnabled(true);
+
+        //aplica uma palheta e cores alternativa no background
+        chart.setBackgroundColor(Color.WHITE);
+        chart.setViewPortOffsets(0f, 0f, 0f, 0f);
+
+        //adiciona a data
+        seekBarX.setProgress(100, true);
+
+        //consegue a legenda da data (apenas depois de definido a data)
+        Legend legenda = chart.getLegend();
+        legenda.setEnabled(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
+//        xAxis.setTypeface(tfLight);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(true);
+        xAxis.setTextColor(Color.rgb(255, 192, 56));
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setGranularity(1f); // one hour
+        xAxis.setValueFormatter(new ValueFormatter() {
+
+            private final SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM HH:mm", Locale.getDefault());
+
+            @Override
+            public String getFormattedValue(float value) {
+
+                long millis = TimeUnit.HOURS.toMillis((long) value);
+                return mFormat.format(new Date(millis));
+            }
+
+        });
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART);
+//        leftAxis.setTypeface(tfLight);
+        leftAxis.setTextColor(ColorTemplate.getHoloBlue());
+        leftAxis.setDrawGridLines(true);
+        leftAxis.setGranularityEnabled(true);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setAxisMaximum(170f);
+        leftAxis.setYOffset(-9f);
+        leftAxis.setTextColor(Color.rgb(255, 192, 56));
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
     }
+
+
+    private void setData(int count, float range) {
+
+        this.count = count;
+        this.range = range;
+
+        // now in hours
+        long now = TimeUnit.MILLISECONDS.toHours(System.currentTimeMillis());
+
+        ArrayList<Entry> values = new ArrayList<>();
+
+        // count = hours
+        float to = now + count;
+
+        // increment by 1 hour
+        for (float x = now; x < to; x++) {
+
+            float y = Util.getRandom(range, 50);
+            values.add(new Entry(x, y)); // add one entry per hour
+        }
+
+        // create a dataset and give it a type
+        LineDataSet set1 = new LineDataSet(values, "DataSet 1");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(ColorTemplate.getHoloBlue());
+        set1.setValueTextColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(1.5f);
+        set1.setDrawCircles(false);
+        set1.setDrawValues(false);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        set1.setDrawCircleHole(false);
+
+        // create a data object with the data sets
+        LineData data = new LineData(set1);
+        data.setValueTextColor(Color.WHITE);
+        data.setValueTextSize(9f);
+
+        // set data
+        chart.setData(data);
+    }
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        tvX.setText(String.valueOf(seekBarX.getProgress()));
+
+        setData(seekBarX.getProgress(), 50);
+
+        // redraw
+        chart.invalidate();
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {}
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {}
+
+
 
 }
